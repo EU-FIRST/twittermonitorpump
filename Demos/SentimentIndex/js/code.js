750cb00dda6f10e21f283bb273f943dd8db7e272
miha <miha@sowalabs.com>
2013-04-08 19:26:45 +0200

    Twitter sentiment demo further development

--- Demos/SentimentIndex/js/code.js
+++ Demos/SentimentIndex/js/code.js
@@ -1,257 +1,432 @@
 var chart;
+
 var MIN_DATE 
-	= Date.UTC(2012, 0, 1); // Jan 1, 2012
+	= Date.UTC(2012, 0, 1); 
 var MAX_DATE 
-	= Date.UTC(2012, 11, 31); // Dec 31, 2012
-var DAY_SPAN
-	= 86400000;
-var FONT
-	= "12px 'Helvetica Neue',Helvetica,Arial,sans-serif";
-var VOL_IDX 
-	= 0; 
-var PRICE_IDX 
+	= Date.UTC(2012, 11, 31); 
+
+var IDX_PRICE
 	= 1;
-var SENT_IDX 
-	= 2; 
-var MA_IDX 
+var IDX_VOL
+	= 2;
+var IDX_COUNT_POS
 	= 3;
+var IDX_COUNT_NEG
+	= 4;
+var IDX_COUNT_DIFF
+	= 5;
+var IDX_COUNT_DIFF_MA7
+	= 6;
+var IDX_COUNT_DIFF_MA14
+	= 7;
+var IDX_POLARITY
+	= 8;
+var IDX_POLARITY_MA7
+	= 9;
+var IDX_POLARITY_MA14
+	= 10;
+var IDX_COUNT
+	= 11;
 
-function dateOnly(ms) {
-	var d = new Date(ms);
-	return Date.UTC(d.getFullYear(), d.getMonth(), d.getDate());
-}
-
-function numFmt(num, d) {
-	return Highcharts.numberFormat(num, d).replace("-", "−");
-}
-
-function filter(data) {
-	var newData = [];
-	for (var i in data) {
-		if (data[i][0] >= MIN_DATE && data[i][0] <= MAX_DATE + DAY_SPAN / 3 * 2) {
-			var d = dateOnly(data[i][0]);
-			newData.push([d, data[i][1]]); 
-		}
+function showChartUpper(option) {
+	var group = [ IDX_PRICE, IDX_VOL ];
+	if (option == "uchart-price") {
+		toggleSeries(chart, group, [ IDX_PRICE ], "Stock price");
+	} else {
+		toggleSeries(chart, group, [ IDX_VOL ], "Trading volume");
 	}
-	return newData;
 }
 
-function align(data, refData) {
-	var newData = [];
-	var v = null;
-	for (var i in refData) {
-		if (data[refData[i][0]]) {
-			newData.push([refData[i][0], v = data[refData[i][0]]]);
-		} else {
-			newData.push([refData[i][0], v]);
-		}
+function showChartLower(option, ma) {
+	var group = [];
+	for (var i = IDX_COUNT_POS; i <= IDX_COUNT; i++) { group.push(i); }
+	if (option == "lchart-diff") {
+		var showSeries = [ IDX_COUNT_POS, IDX_COUNT_NEG ];
+		if (ma == "lchart-ma-none") { showSeries.push(IDX_COUNT_DIFF); }
+		else if (ma == "lchart-ma-7-day") { showSeries.push(IDX_COUNT_DIFF_MA7); }
+		else { showSeries.push(IDX_COUNT_DIFF_MA14); }
+		toggleSeries(chart, group, showSeries, "Absolute difference");
+	} else if (option == "lchart-polar") {
+		var showSeries = [ IDX_POLARITY ];
+		if (ma == "lchart-ma-7-day") { showSeries.push(IDX_POLARITY_MA7); }
+		else if (ma == "lchart-ma-14-day") { showSeries.push(IDX_POLARITY_MA14); }
+		toggleSeries(chart, group, showSeries, "Sentiment polarity");
+	} else {
+		toggleSeries(chart, group, [ IDX_COUNT ], "Tweet count");
 	}
-	return newData;
-}
-
-function MA(data, days) {
-	var maData = [];
-	var queue = [];
-	var sum = 0;
-	for (var i in data) {
-		if (queue.length < days) {
-			queue.push(data[i].y); sum += data[i].y; 
-			maData.push(sum / queue.length);
-		} else {
-			queue.push(data[i].y); sum += data[i].y;
-			sum -= queue.shift();
-			maData.push(sum / days);
-		}
-	}
-	return maData;
-}
-
-function error() {
-	$(".loading-img").hide(); $(".loading-oops").show();
 }
 
 function load(name) {
-	// set initial time span
-	$(".loading-curtain,.loading-img").show();
+	showLoading();
 	$("#datepicker-from").data("datetimepicker").setDate(MIN_DATE);
 	$("#datepicker-to").data("datetimepicker").setDate(MAX_DATE);
-	$("#all").button("toggle");
-	$("#none").button("toggle");
-	$("#occurrence").button("toggle");
-	$.getJSON("volume/" + name + ".txt", function(volume) {
-		volume = filter(volume);
-		$.getJSON("sentiment/" + name + ".txt", function(sentiment) {
-			sentiment = filter(sentiment);
-			$.getJSON("price/" + name + ".txt", function(price) {
-				price = align(price, volume);
-				chart = new Highcharts.StockChart({
-					credits: { 
-						enabled: false 
-					},
-					chart: {
-						renderTo: "chart-container",
-						zoomType: "x",
-					},
-					rangeSelector: { 
-						enabled: false
-					},
-					navigator: {
-						xAxis: {
-							labels: {
-								style: {
-									font: FONT,
-									color: "#000"
-								}
-							}
-						}
-					},
-					xAxis: {
-						events: {
-							setExtremes: function(event) {
-								$("#datepicker-from").data("datetimepicker").setDate(dateOnly(event.min));
-								$("#datepicker-to").data("datetimepicker").setDate(dateOnly(event.max));
-								if (Math.abs(event.max - event.min - chart.span) >= 2 * DAY_SPAN) {
-									chart.span = event.max - event.min;
-									$("#zoom button").removeClass("active");
-								}
-							}
-						},
-						lineColor: "silver",
-						tickColor: "silver",
-						labels: {
-							style: {
-								font: FONT,
-								color: "#000"
-							}
-						}
-					},
-					yAxis: [{
-						title: {
-							style: {
-								font: FONT,
-								color: "#000"
-							},
-							text: "Occurrence"
-						},
-						min: 0,
-						maxPadding: 0,
-						labels: {
-							align: "right",
-							style: {
-								font: FONT,
-								color: "#000"
-							},
-							x: -5,
-						},
-						plotLines: [{
-							value: 0,
-							width: 2,
-							color: "silver"
-						}],
-						height: 200,
-						lineWidth: 2,
-						lineColor: "silver"
-					},
-					{
-						title: {
-							style: {
-								font: FONT,
-								color: "#000"
-							},
-							text: "Sentiment"
-						},
-						maxPadding: 0,
-						minPadding: 0,
-						alignTicks: false,
-						labels: {
-							align: "right",
-							style: {
-								font: FONT,
-								color: "#000"
-							},
-							x: -5,
-							formatter: function () {
-								return (this.value > 0 ? "+" : "") + numFmt(this.value, 0);
-							}
-						},
-						plotLines:[{
-							value: 0,
-							width: 2,
-							color: "silver"
-						}],
-						top: 250,
-						height: 150,
-						offset: 0,
-						lineWidth: 2,
-						lineColor: "silver"
-					}],
-					series: [{
-						name: "Occurrence", 
-						lineWidth: 1,
-						data: volume,
-						states: {
-							hover: {
-								lineWidth: 1
-							}
-						}
-					},
-					{
-						name: "Stock price", 
-						lineWidth: 1,
-						data: price,
-						visible: false,
-						color: "#4572A7",
-						states: {
-							hover: {
-								lineWidth: 1
-							}
-						},
-						marker: { 
-							symbol: "circle" 
-						}
-					},
-					{
-						name: "Sentiment", 
-						yAxis: 1,
-						lineWidth: 1,
-						data: sentiment,
-						states: {
-							hover: {
-								lineWidth: 1
-							}
-						}
-					},
-					{
-						name: "MA", 
-						yAxis: 1,
-						data: sentiment,
-						visible: false,
-						lineWidth: 1,
-						color: "#000",
-						type: "spline",
-						states: {
-							hover: {
-								enabled: false
-							}
-						}
-					}],
-					tooltip: {
-						useHTML: true,
-						formatter: function() {
-							var tooltip = "<span style=\"font-family:'Helvetica Neue',Helvetica,Arial,sans-serif\">" + Highcharts.dateFormat("%a, %b %e, %Y", this.x) + 
-								"<br/><span style=\"color:" + this.points[0].series.color + "\">" + this.points[0].series.name + "</span>: <b>" + numFmt(this.points[0].y, 0) + "</b>" +
-								"<br/><span style=\"color:" + this.points[1].series.color + "\">Sentiment</span>: <b>" + numFmt(this.points[1].y, 2) + "</b>";
-							if (this.points[2]) {
-								tooltip += "<br/><span style=\"color:" + this.points[2].series.color + "\">Sentiment MA</span>: <b>" + numFmt(this.points[2].y, 2) + "</b>";
-							}
-							return tooltip + "</span>";
+	$("#zoom-all,#uchart-price,#lchart-diff,#lchart-ma-none").button("toggle");
+	$.getJSON("data/" + name + ".txt", function(data) {
+		var pointStart = MIN_DATE;
+		var pointInterval = 3600 * 1000 * 24;
+		var vol = aaSum(aaSum(data.obj, data.pos), data.neg);
+		var diff = aaSub(data.pos, data.neg);
+		var pol = aaDiv(diff, asSum(aaSum(data.pos, data.neg), 1));
+		chart = new Highcharts.StockChart({
+			plotOptions: {
+				series: {
+					animation: false
+				}
+			},
+			credits: { 
+				enabled: false 
+			},
+			chart: {
+				renderTo: "chart-container",
+				zoomType: "x",
+			},
+			rangeSelector: { 
+				enabled: false
+			},
+			navigator: {
+				xAxis: {
+					labels: {
+						style: {
+							font: FONT,
+							color: "#000"
 						}
 					}
-				});
-				chart.span = MAX_DATE - MIN_DATE;
-				$(".loading-curtain,.loading-img").hide();
-			}).error(error);
-		}).error(error);
+				}
+			},
+			xAxis: {
+				events: {
+					setExtremes: function(event) {
+						var min = event.min;
+						var max = event.max;
+						if (!min) { min = MIN_DATE; }
+						if (!max) { max = MAX_DATE; }
+						console.log(min);
+						$("#datepicker-from").data("datetimepicker").setDate(dateOnly(min));
+						$("#datepicker-to").data("datetimepicker").setDate(dateOnly(max));
+						if (Math.abs(max - min - chart.span) >= 2 * DAY_SPAN) {
+							chart.span = max - min;
+							$("#zoom button").removeClass("active");
+						}
+					}
+				},
+				lineColor: "silver",
+				tickColor: "silver",
+				labels: {
+					style: {
+						font: FONT,
+						color: "#000"
+					}
+				}
+			},
+			yAxis: [{ // stock price axis
+				title: {
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					text: "Stock price"
+				},
+				maxPadding: 0,
+				minPadding: 0,
+				labels: {
+					align: "right",
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					x: -5,
+				},
+				plotLines: [{
+					value: 0,
+					width: 2,
+					color: "silver"
+				}],
+				height: 200,
+				lineWidth: 2,
+				lineColor: "silver"
+			},
+			{ // trading volume axis
+				title: {
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+				},
+				maxPadding: 0,
+				min: 0,
+				labels: {
+					align: "right",
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					x: -5,
+				},
+				plotLines: [{
+					value: 0,
+					width: 2,
+					color: "silver"
+				}],
+				height: 200,
+				offset: 0,
+				lineWidth: 2,
+				lineColor: "silver"
+			},
+			{ // sentiment difference axis
+				title: {
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					text: "Absolute difference"
+				},
+				maxPadding: 0,
+				minPadding: 0,
+				alignTicks: false,
+				labels: {
+					align: "right",
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					x: -5
+				},
+				plotLines: [{
+					value: 0,
+					width: 2,
+					color: "silver"
+				}],
+				top: 230,
+				height: 200,
+				offset: 0,
+				lineWidth: 2,
+				lineColor: "silver"
+			},
+			{ // sentiment polarity axis
+				title: {
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					text: ""
+				},
+				maxPadding: 0,
+				minPadding: 0,
+				alignTicks: false,
+				labels: {
+					align: "right",
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					x: -5
+				},
+				plotLines: [{
+					value: 0,
+					width: 2,
+					color: "silver"
+				}],
+				top: 230,
+				height: 200,
+				offset: 0,
+				lineWidth: 2,
+				lineColor: "silver"
+			},
+			{ // tweet count axis
+				title: {
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					text: ""
+				},
+				maxPadding: 0,
+				min: 0,
+				alignTicks: false,
+				labels: {
+					align: "right",
+					style: {
+						font: FONT,
+						color: "#000"
+					},
+					x: -5
+				},
+				plotLines: [{
+					value: 0,
+					width: 2,
+					color: "silver"
+				}],
+				top: 230,
+				height: 200,
+				offset: 0,
+				lineWidth: 2,
+				lineColor: "silver"
+			}],
+			series: [{ // this series shows in navigator
+				data: vol,
+				visible: false,
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 1
+				name: "Stock price",
+				decimals: 2,
+				yAxis: 0,
+				lineWidth: 1,
+				data: data.close,
+				visible: true,
+				color: "#4572A7",
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 2
+				name: "Trading volume",
+				decimals: 0,
+				yAxis: 1,
+				min: 0,
+				lineWidth: 1,
+				data: data.vol,
+				visible: false,
+				color: "#4572A7",
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 3
+				name: "Positive tweet count", 
+				decimals: 0,
+				yAxis: 2,
+				lineWidth: 1,
+				type: "area",
+				data: data.pos,
+				visible: true,
+				color: '#89A54E',
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 4
+				name: "Negative tweet count", 
+				decimals: 0,
+				yAxis: 2,
+				lineWidth: 1,
+				type: "area",
+				data: asMult(data.neg, -1),
+				visible: true,
+				color: '#AA4643',
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 5
+				name: "Absolute difference", 
+				decimals: 0,
+				yAxis: 2,
+				lineWidth: 1,
+				color: '#000',
+				data: diff,
+				visible: true,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 6
+				name: "Absolute difference MA", 
+				decimals: 2,
+				yAxis: 2,
+				lineWidth: 1,
+				color: '#000',
+				data: asMult(MA(diff, 7), 2),
+				visible: false,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 7
+				name: "Absolute difference MA", 
+				decimals: 2,
+				yAxis: 2,
+				lineWidth: 1,
+				color: '#000',
+				data: asMult(MA(diff, 14), 2),
+				visible: false,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 8
+				name: "Sentiment polarity", 
+				decimals: 2,
+				yAxis: 3,
+				lineWidth: 1,
+				color: '#AA4643',
+				data: pol, 
+				visible: false,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 9
+				name: "Sentiment polarity MA", 
+				decimals: 2,
+				yAxis: 3,
+				lineWidth: 1,
+				color: '#000',
+				data: MA(pol, 7),
+				visible: false,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 10
+				name: "Sentiment polarity MA", 
+				decimals: 2,
+				yAxis: 3,
+				lineWidth: 1,
+				color: '#000',
+				data: MA(pol, 14),
+				visible: false,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			},
+			{ // 11
+				name: "Tweet count", 
+				decimals: 0,
+				yAxis: 4,
+				lineWidth: 1,
+				color: '#AA4643',
+				data: vol,
+				visible: false,
+				states: { hover: { lineWidth: 1 } },
+				marker: { symbol: "circle" },
+				pointStart: pointStart,
+				pointInterval: pointInterval
+			}],
+			tooltip: {
+				useHTML: true,
+				formatter: function() {
+					var tooltip = "<span style=\"font-family:'Helvetica Neue',Helvetica,Arial,sans-serif\"><b>" + Highcharts.dateFormat("%a, %b %e, %Y", this.x) + "</b>";
+					for (var i in this.points) {
+						tooltip += "<br/><span style=\"color:" + this.points[i].series.color + "\">" + this.points[i].series.name + ":</span> <b>" + Highcharts.numberFormat(this.points[i].y, this.points[i].series.options.decimals) + "</b>";
+					}
+					return tooltip + "</span>";
+				}
+			}
+		});
+		chart.span = MAX_DATE - MIN_DATE;
+		hideLoading();
 	}).error(error);
 }
 
@@ -261,7 +436,7 @@ $(".btn").focus(function() {
 });
 
 // initialize selection box
-$("#entity").val("DAX");
+$("#entity").val("GOOG");
 
 // initialize date pickers
 $("#datepicker-from,#datepicker-to").datetimepicker({
@@ -280,51 +455,42 @@ $("#zoom .btn").click(function() {
 	var action = $(this).attr("id");
 	var span;
 	var dateEnd = chart.xAxis[0].getExtremes().max;
-	if (action == "all") { chart.xAxis[0].setExtremes(MIN_DATE, MAX_DATE); return; }
-	else if (action == "1m") { span = 30 * DAY_SPAN; }
-	else if (action == "3m") { span = 90 * DAY_SPAN; }
-	else if (action == "6m") { span = 180 * DAY_SPAN; }
+	if (action == "zoom-all") { chart.xAxis[0].setExtremes(MIN_DATE, MAX_DATE); return; }
+	else if (action == "zoom-1m") { span = 30 * DAY_SPAN; }
+	else if (action == "zoom-3m") { span = 90 * DAY_SPAN; }
+	else if (action == "zoom-6m") { span = 180 * DAY_SPAN; }
 	var dateStart = dateEnd - span;
 	if (dateStart < MIN_DATE) { dateEnd += (MIN_DATE - dateStart); dateStart += (MIN_DATE - dateStart); }
 	chart.xAxis[0].setExtremes(dateStart, dateEnd);
 });
 
-// assign MA button handlers
-$("#lower-chart .btn").click(function() {
-	var action = $(this).attr("id");
-	if (action == "none") {
-		for (var i in chart.series[MA_IDX].data) { chart.series[MA_IDX].data[i].update(chart.series[SENT_IDX].data[i].y, false); } 
-		chart.redraw();
-		chart.series[MA_IDX].setVisible(false, true);
-	}
-	else { 
-		var data = MA(chart.series[SENT_IDX].data, action == "7-day-avg" ? 7 : 14);
-		for (var i in chart.series[MA_IDX].data) { chart.series[MA_IDX].data[i].update(data[i], false); }
-		chart.series[MA_IDX].setVisible(true, true);
-	}
+// assign upper-chart button handlers
+$("#uchart .btn").click(function() {
+	console.log($(this).attr("id"));
+	showChartUpper($(this).attr("id"));
 });
 
-// assign upper-chart button handlers
-$("#upper-chart .btn").click(function() {
+// assign lower-chart button handlers
+$("#lchart .btn").click(function() {
 	var action = $(this).attr("id");
-	if (action == "price") {
-		chart.series[VOL_IDX].setVisible(false, false);
-		chart.yAxis[0].setOptions($.extend({}, chart.yAxis[0].options, { min: null, title: { text: "Stock price", style: { font: FONT, color: "#000" } } }));
-		chart.yAxis[0].setTitle(); // wtf?!
-		chart.series[PRICE_IDX].setVisible(true, true);
+	if (action == "lchart-count") { 
+		$("#lchart-ma button").addClass("disabled"); 
 	} else {
-		chart.series[PRICE_IDX].setVisible(false, false);
-		chart.yAxis[0].setOptions($.extend({}, chart.yAxis[0].options, { min: 0, title: { text: "Occurrence", style: { font: FONT, color: "#000" } } }));
-		chart.yAxis[0].setTitle(); // wtf?!
-		chart.series[VOL_IDX].setVisible(true, true);
+		$("#lchart-ma button").removeClass("disabled"); 
 	}
+	showChartLower(action, getOption("#lchart-ma"));
+});
+
+// assign MA button handlers
+$("#lchart-ma .btn").click(function() {
+	showChartLower(getOption("#lchart"), $(this).attr("id"));
 });
 
 // assign selection handler 
 $("select").change(function() {
 	$(this)[0].blur(); // rmv ugly focus rectangle
-	load($("select option:selected").attr("value"));
+	load(getSelection("#entity"));
 });
 
 // initialize
-load("GDAXI"); // DAX
+load("GOOG"); 
